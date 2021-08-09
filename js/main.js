78d2caee2aa02cfe1eab3e4fb70479eec5175892
Drew <andrusha.skorlupin@gmail.com>
2021-08-09 23:35:16 +0300

    added fonts

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@ const firstName = 'Drew'
 const lastName = 'Skow'
 const age = 21
 
-console.log('Имя человека ' + firstName + ' старый на ' + age + ' год')
+console.log('Имя человека ' + firstName + ' старый на ' + age + ' год') 
+
+function showMessage(){
+    console.log('qq')
+}
+
+let b = showMessage()
